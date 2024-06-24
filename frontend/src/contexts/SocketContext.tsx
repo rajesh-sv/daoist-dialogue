5555080884e7418d75cb5565534dd89c4299d869
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2024-06-24 17:54:17 +0530

    Add SocketContext

---
+++ frontend/src/contexts/SocketContext.tsx
@@ -0,0 +1,54 @@
+import { SocketContextType } from "@/types";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useAuthContext } from "./AuthContext";
+import io, { Socket } from "socket.io-client";
+
+export const SocketContext = createContext<SocketContextType | null>(null);
+
+export const useSocketContext = () => useContext(SocketContext);
+
+export const SocketContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+  const [onlineUsers, setOnlineUsers] = useState<Set<string>>(new Set());
+  // @ts-ignore
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const newSocket = io("http://localhost:3000", {
+        query: { userId: authUser._id },
+      });
+
+      setSocket(newSocket);
+
+      newSocket.on("onlineUsers", (users) => {
+        setOnlineUsers(new Set(users));
+      });
+
+      return () => {
+        newSocket.close();
+      };
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
